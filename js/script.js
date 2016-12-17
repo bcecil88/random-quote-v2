8877b1722c406049364d2ea2ee41e1b577f1f927
Kyle <bcecil88@gmail.com>
2016-12-17 13:55:07 -0500

    asdf

--- js/script.js
+++ js/script.js
@@ -1,3 +1,47 @@
 // event listener to respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+var quotes = [
+    {
+        quote: '';
+        source: '';
+        citation: '';
+        gender: '';
+    }
+    {
+        quote: '';
+        source: '';
+        citation: '';
+        gender: '';
+    }
+    {
+        quote: '';
+        source: '';
+        citation: '';
+        gender: '';
+    }
+    {
+        quote: '';
+        source: '';
+        citation: '';
+        gender: '';
+    }
+    {
+        quote: '';
+        source: '';
+        citation: '';
+        gender: '';
+    }
+    {
+        quote: '';
+        source: '';
+        citation: '';
+        gender: '';
+    }
+    {
+        quote: '';
+        source: '';
+        citation: '';
+        gender: '';
+    }
+];
